0e527455208f4bf7959ab79031a137f1283eb335
Vladimir Rokvic <vladimir.rokvic@tietoevry.com>
2022-09-13 09:15:23 +0200

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 	//test
 	//test2
+	//test2
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
